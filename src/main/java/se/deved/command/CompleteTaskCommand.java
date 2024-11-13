86a31a032c0f7827b3da65d8a830d320454bbabd
EnanderW <williamenander@gmail.com>
2024-11-13 13:26:53 +0100

    add missing complete task command

--- src/main/java/se/deved/command/CompleteTaskCommand.java
+++ src/main/java/se/deved/command/CompleteTaskCommand.java
@@ -1,6 +1,7 @@
 package se.deved.command;
 
 import se.deved.Application;
+import se.deved.Task;
 
 public class CompleteTaskCommand extends Command {
     public CompleteTaskCommand(Application application) {
@@ -9,6 +10,20 @@ public class CompleteTaskCommand extends Command {
 
     @Override
     public void execute(String[] commandArgs) {
+        if (commandArgs.length != 2) {
+            System.out.println("Usage: complete-task <name>");
+            return;
+        }
 
+        String taskName = commandArgs[1];
+        Task task = application.getTaskManager().getByTitle(taskName);
+        if (task == null) {
+            System.out.println("Den tasken finns inte.");
+            return;
+        }
+
+        task.completed = true;
+        System.out.println("Uppdaterade task '" + task.title + "'");
+        application.getTaskManager().save(task);
     }
 }
